e8522e611cc47f533cb74ac7ac0b6d18f1c3b67d
Omar <omarabdo997@gmail.com>
2020-08-17 16:18:28 +0000

    Controlled components

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  state={
+    query: ''
+  }
+  updateQuery = (event) => {
+    this.setState(() => ({
+      query: event
+    }))
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">ReactND - Coding Practice</h1>
+        </header>
+        <div className="container">
+          <input type="text" placeholder="Say Something" value={this.state.query} onChange={(event)=> this.updateQuery(event.target.value)}/>
+          <p className="echo">Echo:</p>
+          <p>{this.state.query}</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
